9e7b59bcf2bdf37187ba58bcba27a031088610e2
Ove Lindström <ovelindstrom@Ones-MacBook-Pro.local>
2018-03-08 11:06:23 +0100

    Lagt till ett tomt VS.Net Core projekts med nuget dependencies

---
+++ BitCoin/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using QBitNinja.Client;
+
+namespace BitCoin
+{
+    class Program
+    {
+        static string URL = "http://tapi.qbit.ninja";
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello BitCoin!");
+
+        }
+    }
+}
